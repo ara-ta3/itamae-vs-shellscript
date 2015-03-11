66d71c026d2b41ca193f6a4a7d65834632b55672
Arata Tanaka <tarata43@yahoo.co.jp>
2015-03-12 00:26:37 +0900

    vim74を分離した

--- with_itamae/recipes/common.rb
+++ with_itamae/recipes/common.rb
@@ -1,28 +1,5 @@
 package "git"
 package "zsh"
-package "gcc"
-package "mercurial"
-package "ncurses-devel"
-package "lua"
-package "lua-devel"
-
-execute "hg clone https://vim.googlecode.com/hg/ vim" do
-  cwd "/usr/local/src/"
-  not_if "test -d /usr/local/src/vim/src"
-end
-
-execute "./configure --enable-multibyte --with-features=huge --disable-selinux --prefix=/usr/local --enable-luainterp=yes --with-lua-prefix=/usr"do 
-  cwd "/usr/local/src/vim"
-  not_if "test -f /usr/local/bin/vim"
-end
-
-execute "make -C /usr/local/src/vim" do
-  not_if "/usr/local/bin/vim --version|head -n 1|grep 7.4"
-end
-
-execute "make -C /usr/local/src/vim install" do
-  not_if "/usr/local/bin/vim --version|head -n 1|grep 7.4"
-end
 
 execute "usermod -s /bin/zsh vagrant"
 
